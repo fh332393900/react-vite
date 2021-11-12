7d4cd8037b917baf94889a8527436720ea36de86
fenghang <332393900@qq.com>
2021-11-12 17:29:52 +0800

    添加

---
+++ config/index.ts
@@ -0,0 +1,28 @@
+/**
+ * @description 开发端口
+ */
+export const VITE_APP_PORT = 3000;
+/**
+ * @description 公共基础路径
+ */
+export const VITE_APP_BASE = "/";
+/**
+ * @description 是否自动在浏览器中打开应用程序
+ */
+export const VITE_APP_OPEN = true;
+/**
+ * @description 是否在打包环境下，开启打包的分析可视化图
+ */
+export const VITE_APP_VISUALIZER = false;
+/**
+ * @description 是否在打包环境下，去除console.log
+ */
+export const VITE_APP_CONSOLE = true;
+/**
+ * @description 打包环境下，删除debugger
+ */
+export const VITE_APP_DEBUGGER = true;
+/**
+ * @description 打包环境下是否生成source map 文件
+ */
+export const VITE_APP_SOURCEMAP = false;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import createVitePlugins from './config/plugins';
+import cssOption from './config/style';
+
 export default defineConfig((configEnv) => {
   return {
     plugins: createVitePlugins(),
+    css: cssOption
   };
 });
